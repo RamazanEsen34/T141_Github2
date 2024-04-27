f27b2f52ca7b2ee3f559f2e50f1325286836b871
esen <ramazanesen@hotmail.com>
2024-04-27 21:44:48 +0300

    second version

---
+++ src/P02.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class P02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adınızı giriniz :");
+        String isim = scanner.nextLine();
+    }
+}
